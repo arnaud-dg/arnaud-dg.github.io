48bb7b6c9c67aedd2471270b9e77054236a165b0
arnaud_dg <ad_learning@yahoo.com>
2023-11-13 20:10:44 +0000

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var words = ["a pharmacist", "a data enthousiast", "a chemical engineer", "a data scientist", "Ready To Help !"], i = 0;
+setInterval(function() {
+    $("#dynamic-text").fadeOut(function() {
+        $(this).html(words[i = (i + 1) % words.length]).fadeIn();
+    });
+    // Simuler l'effet de frappe
+    $("#dynamic-text").text("");
+    var word = words[i].split("");
+    var typingEffect = setInterval(function() {
+      if (!word.length) {
+        clearInterval(typingEffect);
+        return;
+      }
+      var char = word.shift();
+      $("#dynamic-text").append(char);
+    }, 200);
+}, 2000); // Changez le temps selon la vitesse souhaitée
