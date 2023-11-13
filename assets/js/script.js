203cf74f97acf73e006ce180bbd24ce8504e4236
arnaud_dg <ad_learning@yahoo.com>
2023-11-13 22:19:22 +0000

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 // Initialisation des mots
-var words = ["a pharmacist", "a data enthusiast", "a chemical engineer", "a data scientist", "Ready To Help You!"];
+var words = ["a pharmacist", "a data enthusiast", "a chemical engineer", "a data scientist", "Ready To Help !"];
 var i = 0;
+var typingSpeed = 100; // Vitesse de frappe
+var deletingSpeed = typingSpeed / 3; // Vitesse d'effacement, 3 fois plus rapide
 
 function typeEffect() {
     var word = words[i].split("");
     var typingEffect = setInterval(function() {
         if (!word.length) {
-            clearInterval(typingEffect); // Arrêter l'effet de frappe
-            i = (i + 1) % words.length; // Passer au mot suivant
-            setTimeout(changeWord, 2000); // Attendre avant de changer de mot
+            clearInterval(typingEffect);
+            setTimeout(deleteEffect, 1000); // Attendre 1000ms avant de commencer à effacer
             return;
         }
         var char = word.shift();
         $("#dynamic-text").append(char);
-    }, 100); // Vitesse de frappe
+    }, typingSpeed);
+}
+
+function deleteEffect() {
+    var word = $("#dynamic-text").text();
+    var deletingEffect = setInterval(function() {
+        if (!word.length) {
+            clearInterval(deletingEffect);
+            i = (i + 1) % words.length;
+            setTimeout(changeWord, 500); // Temps avant de changer de mot
+            return;
+        }
+        word = word.slice(0, -1); // Enlever un caractère
+        $("#dynamic-text").text(word);
+    }, deletingSpeed);
 }
 
 function changeWord() {
     $("#dynamic-text").fadeOut(function() {
-        $(this).text(""); // Effacer le texte actuel
-        $(this).fadeIn(typeEffect); // Démarrer l'effet de frappe après le fondu
+        $(this).text("");
+        $(this).fadeIn(typeEffect);
     });
 }
 
-// Démarrer le processus
 changeWord();
+
