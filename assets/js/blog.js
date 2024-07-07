e558792762b27cee862f0b9a05cbdef4371f7ff6
arnaud_dg <ad_learning@yahoo.com>
2024-07-07 16:38:32 +0200

    Update blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,12 +2,17 @@ const githubUsername = 'arnaud-dg';
 const repoName = 'arnaud-dg.github.io';
 
 async function fetchBlogPosts() {
-    const response = await fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/blog`);
+    const response = await fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/contents/blog`);
     const files = await response.json();
-    const blogPosts = files.map(file => {
-        const [date, title] = file.name.replace('.md', '').split(' - ');
-        return { date, title, link: `blog/${file.name}` };
-    });
+    const blogPosts = files
+        .filter(file => file.name.endsWith('.md'))
+        .map(file => {
+            const [datePart, ...titleParts] = file.name.replace('.md', '').split(' - ');
+            const [day, month, year] = datePart.split('_');
+            const date = `${day}/${month}/${year}`;
+            const title = titleParts.join(' - ');
+            return { date, title, link: `blog/${file.name}` };
+        });
 
     return blogPosts;
 }
