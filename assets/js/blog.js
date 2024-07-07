34372baee8f6c98bb267f30b15503740c5e3f988
arnaud_dg <ad_learning@yahoo.com>
2024-07-07 16:49:46 +0200

    Update blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,16 +2,16 @@ const githubUsername = 'arnaud-dg';
 const repoName = 'arnaud-dg.github.io';
 
 async function fetchBlogPosts() {
-    const response = await fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/contents/blog`);
+    const response = await fetch('https://api.github.com/repos/${githubUsername}/${repoName}/contents/blog');
     const files = await response.json();
     const blogPosts = files
         .filter(file => file.name.endsWith('.md'))
         .map(file => {
             const [datePart, ...titleParts] = file.name.replace('.md', '').split(' - ');
             const [day, month, year] = datePart.split('_');
-            const date = `${day}/${month}/${year}`;
+            const date = '${day}/${month}/${year}';
             const title = titleParts.join(' - ');
-            return { date, title, link: `blog/${file.name}` };
+            return { date, title, link: 'blog/${file.name}' };
         });
 
     return blogPosts;
@@ -26,7 +26,7 @@ function renderPosts(blogPosts) {
     currentPosts.forEach(post => {
         const postDiv = document.createElement('div');
         postDiv.classList.add('blog-post');
-        postDiv.innerHTML = `<p>${post.date}</p><a href="${post.link}">${post.title}</a>`;
+        postDiv.innerHTML = '<p>${post.date}</p><a href="${post.link}">${post.title}</a>';
         blogContainer.appendChild(postDiv);
     });
 }
